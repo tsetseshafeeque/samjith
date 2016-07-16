72e465e2c8b22272a79056da83599f859f267237
Samjith A Hameed <Samjith A Hameed@Samjith>
2016-07-16 11:39:32 +0530

    my first committ

---
+++ gitlabs/src/gitlabs/HelloWorld.java
@@ -0,0 +1,14 @@
+package gitlabs;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello, World");
+		System.out.println("Samjith, Hameed");
+		
+		
+	}
+
+}
