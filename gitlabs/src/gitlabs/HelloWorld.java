ece0b93ddf1ab6b440f6ad9491c02ff43794ed4b
tsetseshafeeque <tsetseshafeeque@gmail.com>
2016-07-18 22:34:32 +0530

    nawaf
    nawaf changed it

--- gitlabs/src/gitlabs/HelloWorld.java
+++ gitlabs/src/gitlabs/HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld {
 		System.out.println("Hello, World");
 		System.out.println("Samjith, Hameed");
 		System.out.println("Samjith, shafeeque");
+		System.out.println("Samjith, nawaf");
 		
 		
 	}
